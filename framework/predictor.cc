8755f4c49b5ce3a121e6a2cb1b1b6be078fb401a
Hunter Drake <hgd@ruby.cecs.pdx.edu>
2024-02-24 23:49:55 -0800

    get_prediction() is rough drafted. Added support for the various Alpha tables and predictors in the PREDICTOR class declared in predictor.h

--- framework/predictor.cc
+++ framework/predictor.cc
@@ -1,4 +1,4 @@
-/* Author: Mark Faust;   
+/* Author: Mark Faust;
  * Description: This file defines the two required functions for the branch predictor.
 */
 
@@ -8,15 +8,57 @@
 
     bool PREDICTOR::get_prediction(const branch_record_c* br, const op_state_c* os)
         {
-		/* replace this code with your own */
-            bool prediction = false;
 
-			printf("%0x %0x %1d %1d %1d %1d ",br->instruction_addr,
-			                                br->branch_target,br->is_indirect,br->is_conditional,
-											br->is_call,br->is_return);
-            if (br->is_conditional)
-                prediction = true;
-            return prediction;   // true for taken, false for not taken
+		// true = predicting taken, false = predicting not taken
+		bool local_prediction;		// local prediction result
+		bool global_prediction;		// global prediction result
+		bool choice_prediction; 	// tournament choice (false = local, true = global)
+
+		// Get local history for the current branch and make sure the history is only 10 bits wide
+		uint16_t curr_local_history = local_history_table[((br->instruction_addr & 0xFFF) >> 2)];
+		curr_local_history &= 0x3FF;
+
+		// Get local prediction
+		if ((local_predictor[curr_local_history] & 0x4) > 0) {
+			local_prediction = true;
+		}
+		else {
+			local_prediction = false;
+		}
+
+		// Get global prediction
+		if ((global_predictor[(path_history_table & 0xFFF)] & 0x2) > 0) {
+			global_prediction = true;
+		}
+		else {
+			global_prediction = false;
+		}
+
+		// Get choice prediction
+		if ((choice_predictor[(path_history_table & 0xFFF)] & 0x2) > 0){
+			choice_prediction = true;
+		}
+		else {
+			choice_prediction = false;
+		}
+
+		// Get branch prediction
+		if (choice_prediction) {
+			return global_prediction;
+		}
+		else {
+			return local_prediction;
+		}
+
+		//void init();		 	// init the branch record
+		//void debug_print(); 		// print info in branch record (for debugging)
+		//uint instruction_addr; 	// the branch's PC (program counter)
+		//uint branch_target; 		// target of branch if it's taken; unconditional are always taken
+		//uint instruction_next_addr;	// PC of the static instruction following the branch
+		//bool is_indirect; 		// true if target is computed; false if it's PC-rel; returns are also considered indirect
+		//bool is_conditional; 		// true if branch is conditional; false otherwise
+		//bool is_call; 		// true if branch is a call; false otherwise
+		//bool is_return; 		// true if branch is a return; false otherwise
         }
 
 
--- framework/predictor.h
+++ framework/predictor.h
@@ -10,7 +10,7 @@
 #include <cstring>
 #include <inttypes.h>
 #include <vector>
-#include "op_state.h"   // defines op_state_c (architectural state) class 
+#include "op_state.h"   // defines op_state_c (architectural state) class
 #include "tread.h"      // defines branch_record_c class
 
 class PREDICTOR
@@ -20,7 +20,19 @@ public:
 
     void update_predictor(const branch_record_c* br, const op_state_c* os, bool taken);
 
+    //Convention 1?: LSB is history tables are the most recent bit added to the history
+    //Convention 2?: Predictor counter state values are in the least significant bits
+
+    // 1024 x 10 bit local history table
+    uint16_t local_history_table[1024];
+    // 1024 x 3 bit local predictor (3 bit saturating counter)
+    uint8_t local_predictor[1024];
+    // 1 x 12 bit path history table
+    uint16_t path_history_table;
+    // 4096 x 2 bit global predictor (2 bit saturating counter)
+    uint8_t global_predictor[4096];
+    // 4096 x 2 bit choice predictor (2 bit saturating counter)
+    uint8_t choice_predictor[4096];
 };
 
 #endif // PREDICTOR_H_SEEN
-
