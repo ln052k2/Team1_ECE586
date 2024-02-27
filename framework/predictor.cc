7ed5076d83a83f921912fe16863e89cef060a63c
Lynn <ln05.2k2@gmail.com>
2024-02-27 13:22:44 -0800

    Initial commit, just framework.

--- framework/predictor.cc
+++ framework/predictor.cc
@@ -8,48 +8,7 @@
 
     bool PREDICTOR::get_prediction(const branch_record_c* br, const op_state_c* os)
         {
-
-		// true = predicting taken, false = predicting not taken
-		bool local_prediction;		// local prediction result
-		bool global_prediction;		// global prediction result
-		bool choice_prediction; 	// tournament choice (false = local, true = global)
-
-		// Get local history for the current branch and make sure the history is only 10 bits wide
-		uint16_t curr_local_history = local_history_table[((br->instruction_addr & 0xFFF) >> 2)];
-		curr_local_history &= 0x3FF;
-
-		// Get local prediction
-		if ((local_predictor[curr_local_history] & 0x4) > 0) {
-			local_prediction = true;
-		}
-		else {
-			local_prediction = false;
-		}
-
-		// Get global prediction
-		if ((global_predictor[(path_history_table & 0xFFF)] & 0x2) > 0) {
-			global_prediction = true;
-		}
-		else {
-			global_prediction = false;
-		}
-
-		// Get choice prediction
-		if ((choice_predictor[(path_history_table & 0xFFF)] & 0x2) > 0){
-			choice_prediction = true;
-		}
-		else {
-			choice_prediction = false;
-		}
-
-		// Get branch prediction
-		if (choice_prediction) {
-			return global_prediction;
-		}
-		else {
-			return local_prediction;
-		}
-
+			
 		//void init();		 	// init the branch record
 		//void debug_print(); 		// print info in branch record (for debugging)
 		//uint instruction_addr; 	// the branch's PC (program counter)
